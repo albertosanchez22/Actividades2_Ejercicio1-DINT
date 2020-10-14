da7680751445a4e016ab77379a511ab9442396bd
Alberto Sánchez <alberto.sanchez@iesdoctorbalmis.com>
2020-10-14 10:28:15 +0200

    Terminado prueba

---
+++ Ejercicio1/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+       static Random rnd = new Random();
+        int aleatorio = rnd.Next(0, 100);
+
+        private void ComprobarBoton(object sender, RoutedEventArgs e)
+        {
+            
+            
+            try
+            {
+                if (int.Parse(TextNumero.Text) > aleatorio)
+                {
+                    MayorMenor.Text = "El numero es menor";
+                }
+                else if (int.Parse(TextNumero.Text) < aleatorio)
+                {
+                    MayorMenor.Text = "El numero es mayor";
+                }else MayorMenor.Text = "Enhorabuena numero correcto";
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        private void ReiniciarBoton(object sender,RoutedEventArgs e)
+        {
+            MayorMenor.Text = "";
+            TextNumero.Text = "";
+            aleatorio = rnd.Next(0, 100); 
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
